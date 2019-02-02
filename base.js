30bdfef26d7ae22827a62e2fcc39344ce0e5849f
BhanuSaiKumarSV <bhanusai.sv@ggktech.com>
2019-02-02 15:24:50 +0530

    adding more

--- base.js
+++ base.js
@@ -1,25 +1,62 @@
 var optionCount = 0;
+var formData = []
 
 validateData = (formData) => {
+    let options={};
     const {name, type, required, order} = formData;
     if (name=="" || order=="") {
         return false;
     }
-    return true;
+    switch(type) {
+        case 'number': 
+            let minRange = document.getElementById("min-range").value;
+            let maxRange = document.getElementById("max-range").value;
+            if (minRange=="" || maxRange=="" || minRange>maxRange || minRange == maxRange) {
+                return false;
+            }
+            options.minRange = minRange;
+            options.maxRange = maxRange;
+            break;
+        case 'checkboxes':
+        case 'select':
+        case 'radio':
+            let tempCount = optionCount;
+            while(tempCount){
+                let name = document.getElementById(`name${optionCount}`).value;
+                let value = document.getElementById(`value${optionCount}`).value;
+                if (name=="" || value =="") {
+                    return false;
+                }
+                tempCount-- ;
+                options[name] = value;
+            }
+            break;
+        case 'text':
+        case 'textarea':
+            if(document.getElementById("regex").value !== ''){
+                options.regex = document.getElementById("regex").value;
+            }
+    }
+    return options;
 }
 
 document.getElementById("create").addEventListener("click", function(e){
-    var optionCount = 0;    e.preventDefault();    //stop form from submitting
-    let formData = {
+    var optionCount = 0;      //stop form from submitting
+    let fieldData = {
         name: document.getElementById("newField-name").value,
         required: document.getElementById("newField-required").checked,
         order: document.getElementById("newField-order").value,
         type: document.getElementById("newField-type").value,
     }
-    if(!validateData(formData)) {
-        alert("Add proper name and order")
+    let validatedData = validateData(fieldData);
+    if(!validatedData) {
+        alert("Not all fields are added")
+        return;
+        e.preventDefault(); 
     }
-    debugger;
+    fieldData.options = validatedData;
+    formData.push(fieldData);
+    document.getElementById("right-pane").innerHTML = JSON.stringify(formData);
 });
 
 typeChange=()=>{
@@ -29,8 +66,6 @@ typeChange=()=>{
     let options;
     switch(type) {
         case 'text': 
-            options = `<label for="regex">Regex:</label> <input type="text" class="form-control" id="regex">`;
-            break;
         case 'textarea': 
             options = `<label for="regex">Regex:</label> <input type="text" class="form-control" id="regex">`;
             break;
@@ -40,12 +75,8 @@ typeChange=()=>{
                         <br>Regex:<input type="text" class="form-control" id="regex">`;
             break;
         case 'checkboxes':
-            options = `<button onclick="addOption()">Add Option</button>`
-            break;
-        case 'select':
-            options = `<button onclick="addOption()">Add Option</button>`
-            break;
         case 'radio':
+        case 'select':
             options = `<button onclick="addOption()">Add Option</button>`
             break;
         default:
